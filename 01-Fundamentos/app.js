7dae67f1cd2112429c61f1db2c9a619f1b9e1918
gatoguapo <victor._espinoza@hotmail.com>
2023-06-25 22:55:59 -0700

    cambio al valor de las variables

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -2,8 +2,11 @@
 
 let a = 10, 
     b = 20, 
-    c = 10, 
-    d = 10, 
+    c = 30, 
+    d = 40, 
     x = a + b;  
 
-console.log(x)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
