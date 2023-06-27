8b89324f18b7335215b6d55a2337b5cc16e4d56e
gatoguapo <victor._espinoza@hotmail.com>
2023-06-26 20:19:24 -0700

    exercise with all primitives datas and typeof

--- 01-Fundamentos/assets/js/primitivos.js
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -8,9 +8,29 @@ nombre = "Miles Morales";
 nombre = `Miles Morales`;
 
 console.log(nombre);
-
 console.log(typeof(nombre));
 
 nombre = 123;
-
 console.log(typeof(nombre));
+
+let esMarvel = true;
+console.log(typeof(esMarvel));
+
+let edad = 33;
+console.log(typeof(edad));
+
+edad = 33.0000000231312
+console.log(typeof(edad));
+
+let superPoder;
+console.log(typeof(superPoder));
+
+let soyNull = null;
+console.log(typeof(soyNull));
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof(symbol1));
+
+console.log( symbol1 === symbol2);
