1e830b0b811ef1241e721f1b9268ddfd884aec36
gatoguapo <victor._espinoza@hotmail.com>
2023-06-27 20:19:50 -0700

    array unshift method

--- 01-Fundamentos/assets/js/arreglos2.js
+++ 01-Fundamentos/assets/js/arreglos2.js
@@ -3,7 +3,6 @@ console.log(juegos.length);
 
 let primero = juegos[0];
 let ultimo = juegos[juegos.length-1];
-
 console.log(primero, ultimo);
 
 juegos.forEach((juego,indice,arr)=> {
@@ -11,5 +10,7 @@ juegos.forEach((juego,indice,arr)=> {
 });
 
 let nuevaLong = juegos.push('F-Zero');
+console.log(nuevaLong, juegos);
 
-console.log(nuevaLong, juegos)
+nuevaLong = juegos.unshift('Minecraft')
+console.log(nuevaLong, juegos);
