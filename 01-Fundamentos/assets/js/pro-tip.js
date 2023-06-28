bc48574733b4f55edcc5ff3873607ba36e4e9e7b
gatoguapo <victor._espinoza@hotmail.com>
2023-06-28 14:38:16 -0700

    lambda function with return

---
+++ 01-Fundamentos/assets/js/pro-tip.js
@@ -0,0 +1,7 @@
+// function crearPersona(nombre, apellido) {
+//     return { nombre, apellido }
+// }
+const crearPersona = (nombre, apellido) => ({nombre, apellido}) 
+
+const persona = crearPersona('Fernando', 'Herrera')
+console.log(persona)
