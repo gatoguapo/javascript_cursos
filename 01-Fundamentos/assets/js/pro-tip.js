e9bf8fc0e73eb8c068812551ee1a23125c8763cb
gatoguapo <victor._espinoza@hotmail.com>
2023-06-28 14:56:21 -0700

    lambda function imprimeArgumentos

--- 01-Fundamentos/assets/js/pro-tip.js
+++ 01-Fundamentos/assets/js/pro-tip.js
@@ -5,3 +5,15 @@ const crearPersona = (nombre, apellido) => ({nombre, apellido})
 
 const persona = crearPersona('Fernando', 'Herrera')
 console.log(persona)
+
+function imprimeArgumentos() {
+    console.log(arguments)
+}
+
+const imprimeArgumentos2 = (edad, ...args) => (args)
+
+const [casado, vivo, nombre, saludo] = imprimeArgumentos2(10, true, false, 'Ramirez', 'Hola')
+console.log({casado, vivo, nombre, saludo})
+
+const {apellido: nuevoApellido} = crearPersona("Oscar", "Ramirez")
+console.log({nuevoApellido})
