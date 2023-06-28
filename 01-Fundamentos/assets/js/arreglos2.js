fe0229cf0977fd484d3b3e4768d56c21b8e0d5cc
gatoguapo <victor._espinoza@hotmail.com>
2023-06-27 20:22:11 -0700

    array pop method

--- 01-Fundamentos/assets/js/arreglos2.js
+++ 01-Fundamentos/assets/js/arreglos2.js
@@ -7,10 +7,13 @@ console.log(primero, ultimo);
 
 juegos.forEach((juego,indice,arr)=> {
     console.log({juego,indice,arr})
-});
+})
 
 let nuevaLong = juegos.push('F-Zero');
 console.log(nuevaLong, juegos);
 
 nuevaLong = juegos.unshift('Minecraft')
 console.log(nuevaLong, juegos);
+
+let elementoBorrado = juegos.pop();
+console.log(elementoBorrado, juegos)
