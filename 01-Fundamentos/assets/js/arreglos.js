e4b12e049a135e2d04f941dd912fb0655bee07cd
gatoguapo <victor._espinoza@hotmail.com>
2023-06-27 19:58:00 -0700

    arregloCosas

--- 01-Fundamentos/assets/js/arreglos.js
+++ 01-Fundamentos/assets/js/arreglos.js
@@ -6,3 +6,16 @@
 let videojuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
 console.log({videojuegos})
 console.log(videojuegos[0])
+
+let arregloCosas = [
+    true,
+    123,
+    'Ramirez',
+    1 + 2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ['X', 'Megaman', 'Zero', 'Dr.Light']
+];
+
+console.log({arregloCosas})
