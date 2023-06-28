0ca1d708f838dd6266ea02735394688c9ef9c437
gatoguapo <victor._espinoza@hotmail.com>
2023-06-27 22:34:20 -0700

    anonymous function in lamda

--- 01-Fundamentos/assets/js/funciones.js
+++ 01-Fundamentos/assets/js/funciones.js
@@ -1,10 +1,16 @@
 function saludar(nombre) {
     console.log('Hola '+nombre)
+    console.log(arguments)
 }
 
 const saludar2 = function(nombre) {
     console.log('Hola '+nombre)
 }
 
-saludar('Ramirez');
+const saludarFlecha = (nombre) => {
+    console.log('Hola '+nombre)
+}
+
+saludar('Ramirez', 40, true, 'Costa Rica');
 saludar2('Jorge')
+saludarFlecha('ESPARZA')
