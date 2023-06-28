02cca2fe5d2e2bd9e3fd8cc9a5e666729c2a4b02
gatoguapo <victor._espinoza@hotmail.com>
2023-06-27 19:51:38 -0700

    Array videojuegos

--- 01-Fundamentos/assets/js/arreglos.js
+++ 01-Fundamentos/assets/js/arreglos.js
@@ -1,4 +1,8 @@
-const arr = new Array(10);
-const arr2 = [];
-console.log(arr);
-console.log(arr2);
+// const arr = new Array(10);
+// const arr2 = [];
+// console.log(arr);
+// console.log(arr2);
+
+let videojuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log({videojuegos})
+console.log(videojuegos[0])
