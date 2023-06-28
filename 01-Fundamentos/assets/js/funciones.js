705af280596ef0e0e275585e791a8253a306c594
gatoguapo <victor._espinoza@hotmail.com>
2023-06-27 22:39:27 -0700

    first return function

--- 01-Fundamentos/assets/js/funciones.js
+++ 01-Fundamentos/assets/js/funciones.js
@@ -1,6 +1,7 @@
 function saludar(nombre) {
     console.log('Hola '+nombre)
     console.log(arguments)
+    return 10
 }
 
 const saludar2 = function(nombre) {
@@ -11,6 +12,7 @@ const saludarFlecha = (nombre) => {
     console.log('Hola '+nombre)
 }
 
-saludar('Ramirez', 40, true, 'Costa Rica');
+const retornoDeSaludar = saludar('Ramirez', 40, true, 'Costa Rica');
+console.log({retornoDeSaludar})
 saludar2('Jorge')
 saludarFlecha('ESPARZA')
