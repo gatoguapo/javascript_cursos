3409bb025a7037b67e104d86a0b503bb640101de
gatoguapo <victor._espinoza@hotmail.com>
2023-06-28 21:48:09 -0700

    ifs with days of the week

--- 01-Fundamentos/assets/js/if-else.js
+++ 01-Fundamentos/assets/js/if-else.js
@@ -11,3 +11,19 @@ if (a >= 10) {
 const hoy = new Date()
 let dia = hoy.getDay()
 console.log({dia})
+
+if (dia === 0) {
+    console.log('Domingo')
+} else if (dia === 1) {
+    console.log('Lunes')
+} else if (dia === 2) {
+    console.log('Martes')
+} else if (dia === 3) {
+    console.log('Miercoles')
+} else if (dia === 4) {
+    console.log('Jueves')
+} else if (dia === 5) {
+    console.log('Viernes')
+} else if (dia === 6) {
+    console.log('Sabado')
+}
