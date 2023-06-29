ad4e3dd422239099fbaac5f32ccfab80bb659f92
gatoguapo <victor._espinoza@hotmail.com>
2023-06-28 22:31:50 -0700

    or operator

--- 01-Fundamentos/assets/js/logica-booleana.js
+++ 01-Fundamentos/assets/js/logica-booleana.js
@@ -14,7 +14,8 @@ console.log(!true) //false
 console.log(!false) //true
 console.log('!null ', !null) //true
 console.log('!undefined ', !undefined) //true
-
+console.log('undefined == null?')
+console.log(undefined == null)
 console.log(!regresaFalse())
 
 console.warn('And')
@@ -24,3 +25,8 @@ console.log("----------------------")
 console.log(regresaFalse() && regresaTrue()) //false
 console.log(regresaTrue() && regresaFalse()) //false
 console.log(!regresaFalse() && regresaTrue()) //true
+
+console.warn('Or') //true
+console.log(true || false)
+console.log(false || false)
+console.log(regresaTrue() || regresaFalse())
