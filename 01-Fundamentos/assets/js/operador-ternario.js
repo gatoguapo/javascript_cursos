3a8ad693f0af4f709c4afa08db903a641850e592
gatoguapo <victor._espinoza@hotmail.com>
2023-06-29 12:43:07 -0700

    correct way to string concat

--- 01-Fundamentos/assets/js/operador-ternario.js
+++ 01-Fundamentos/assets/js/operador-ternario.js
@@ -4,3 +4,24 @@
  */
 
 // Entra a un sitio web, para consultar si esta abierto hoy
+const dia = 0
+const horaActual = 10
+
+let horaApertura
+let mensaje
+
+if (dia === 0 || dia === 6) {
+    console.log('Fin de semana')
+    horaApertura = 9
+} else {
+    console.log('Dia de semana')
+    horaApertura = 11
+}
+
+if (horaActual >= horaApertura) {
+    mensaje = 'Esta abierto'
+} else {
+    mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura}`
+}
+
+console.log({horaApertura, mensaje})
