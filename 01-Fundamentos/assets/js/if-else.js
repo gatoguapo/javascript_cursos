5eb418fb52e90d49ce443cf76c4a2b47b51440e6
gatoguapo <victor._espinoza@hotmail.com>
2023-06-28 21:39:40 -0700

    Date object

--- 01-Fundamentos/assets/js/if-else.js
+++ 01-Fundamentos/assets/js/if-else.js
@@ -6,4 +6,8 @@ if (a >= 10) {
     console.log('"a" es menor a 10')
 }
 
-console.log('Fin del programa')
+// console.log('Fin del programa')
+
+const hoy = new Date()
+let dia = hoy.getDay()
+console.log({dia})
