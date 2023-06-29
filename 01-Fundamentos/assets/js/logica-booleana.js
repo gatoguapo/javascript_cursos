5814d6f07e1b594215a2c1aca0d8508e408a02dc
gatoguapo <victor._espinoza@hotmail.com>
2023-06-29 12:28:22 -0700

    assignments with or operator

--- 01-Fundamentos/assets/js/logica-booleana.js
+++ 01-Fundamentos/assets/js/logica-booleana.js
@@ -39,5 +39,8 @@ const soyFalse = false
 
 const a1 = true && 'Hola mundo' && 150
 const a2 = 'Hola' && 'Mundo' && soyFalse && true
+const a3 = soyFalse || 'Ya no soy false'
+const a4 = soyFalse || soyUndefined || soyNull || 'Ya no soy false de nuevo' || true
+const a5 = soyFalse || soyUndefined || regresaTrue() || 'Ya no soy false de nuevo' || true
 
-console.log({a1, a2})
+console.log({a1, a2, a3, a4, a5})
