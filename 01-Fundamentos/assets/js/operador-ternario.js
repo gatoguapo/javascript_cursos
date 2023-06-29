23ca8470e91c23d1483ca5da6edc7a8ddfc9ccc0
gatoguapo <victor._espinoza@hotmail.com>
2023-06-29 12:51:49 -0700

    ternary operator first example

--- 01-Fundamentos/assets/js/operador-ternario.js
+++ 01-Fundamentos/assets/js/operador-ternario.js
@@ -4,25 +4,27 @@
  */
 
 // Entra a un sitio web, para consultar si esta abierto hoy
-const dia = 6
-const horaActual = 11
+const dia = 1
+const horaActual = 10
 
 let horaApertura
 let mensaje
 
 //if (dia === 0 || dia === 6) {
-if ( [0,6].includes(dia)) {
-    console.log('Fin de semana')
-    horaApertura = 9
-} else {
-    console.log('Dia de semana')
-    horaApertura = 11
-}
+// if ( [0,6].includes(dia)) {
+//     console.log('Fin de semana')
+//     horaApertura = 9
+// } else {
+//     console.log('Dia de semana')
+//     horaApertura = 11
+// }
+horaApertura = ([0,6].includes(dia)) ? 9 : 11
 
-if (horaActual >= horaApertura) {
-    mensaje = 'Esta abierto'
-} else {
-    mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura}`
-}
+// if (horaActual >= horaApertura) {
+//     mensaje = 'Esta abierto'
+// } else {
+//     mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura}`
+// }
+mensaje = (horaActual >= horaApertura) ? 'Esta abierto' : `Esta cerrado hoy abrimos a las ${horaApertura}`
 
 console.log({horaApertura, mensaje})
