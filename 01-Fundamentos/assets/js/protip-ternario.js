ed47246619408ef218a339d393bdf69f4a9bb268
gatoguapo <victor._espinoza@hotmail.com>
2023-06-29 13:05:25 -0700

    ternary operator in array

--- 01-Fundamentos/assets/js/protip-ternario.js
+++ 01-Fundamentos/assets/js/protip-ternario.js
@@ -5,3 +5,14 @@ const tieneMembresia = (miembro) => (miembro) ? '2 dolares' : '10 dolares'
 console.log(elMayor(20,15))
 console.log(tieneMembresia(false))
 
+const amigo = false
+const amigos = [
+    'Peter',
+    'Tony',
+    'Dr.Strange',
+    amigo ? 'Thor' : 'Loki',
+    // (() => 'Nick Fury')()
+    elMayor(10,15)
+]
+
+console.log({amigos})
