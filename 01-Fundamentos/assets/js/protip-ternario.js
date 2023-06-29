12567b36502b848e6c63524d20fb71bd55a7b704
gatoguapo <victor._espinoza@hotmail.com>
2023-06-29 13:03:19 -0700

    ternary operator in lambda functions

---
+++ 01-Fundamentos/assets/js/protip-ternario.js
@@ -0,0 +1,7 @@
+const elMayor = (a, b) => (a > b) ? a : b
+
+const tieneMembresia = (miembro) => (miembro) ? '2 dolares' : '10 dolares'
+
+console.log(elMayor(20,15))
+console.log(tieneMembresia(false))
+
