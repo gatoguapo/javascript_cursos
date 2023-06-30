77003b45b6c950f419f594dbac5d2f01402e9204
gatoguapo <victor._espinoza@hotmail.com>
2023-06-30 15:40:10 -0700

    for off example

--- 01-Fundamentos/assets/js/ciclos/for.js
+++ 01-Fundamentos/assets/js/ciclos/for.js
@@ -6,6 +6,11 @@ for(i=0; i<heroes.length; i++) {
 }
 
 console.warn('For in')
-for (i in heroes) {
+for(i in heroes) {
     console.log(heroes[i])
+}
+
+console.warn('For of')
+for(heroe of heroes) {
+    console.log(heroe)
 }
