71050a8e5d77d4651f6020b548af773bdc77ee89
gatoguapo <victor._espinoza@hotmail.com>
2023-07-04 19:02:35 -0700

    underscore.js fixed

--- 02-Blackjack/assets/js/juego.js
+++ 02-Blackjack/assets/js/juego.js
@@ -1,1 +1,23 @@
 let deck = []
+const types = ['C', 'D', 'H', 'S'] 
+const others = ['A', 'J', 'Q', 'K']
+
+const createDeck = () => {
+    for (i = 2; i<=10; i++) {
+        for (type in types) {
+            deck.push( i + types[type])
+        }
+    }
+
+    for (type in types) {
+        for (other in others) {
+            deck.push( others[other] + types[type])
+        }
+    }
+
+    
+}
+
+createDeck()
+
+console.log(deck)
