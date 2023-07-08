78dfaad807748f22e9631b180aac80b1f3ee7984
gatoguapo <victor._espinoza@hotmail.com>
2023-07-07 19:27:40 -0700

    btnNewGame reference created

--- 02-Blackjack/assets/js/juego.js
+++ 02-Blackjack/assets/js/juego.js
@@ -8,6 +8,8 @@ let computerScore = 0
 //References
 const btnHit = document.querySelector('#btnHit')
 const btnStop = document.querySelector('#btnStop')
+const btnNewGame = document.querySelector('#btnNewGame')
+
 const divPlayerCards = document.querySelector('#player-cards')
 const divComputerCards = document.querySelector('#computer-cards')
 let smalls = document.querySelectorAll('small')
